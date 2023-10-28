3f50bf82a0d2af334ff61e521d5447866f45d721
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2023-10-28 10:06:50 +0800

    a
    a

--- Assets/Scripts/Player/playercomponent.cs
+++ Assets/Scripts/Player/playercomponent.cs
@@ -37,55 +37,25 @@ public class playercomponent : MonoBehaviour
     bool canDash = true;
     bool isDashing;
 
-    #region Jump Header
-    //[Header("Jump")]
-    //[SerializeField] private float jumpForce;
-    //[SerializeField] private float jumpCoolDown;
-    //[SerializeField] private float airMultiplier;
-    //private bool readyToJump;
-    #endregion
-
     private Vector3 moveDir;
-    // [SerializeField] float footstepOffset;
 
     [Header("Camera")]
     [SerializeField] private Transform cameraObject;
 
-    //[SerializeField] Camera cam;
-    //[SerializeField] private Vector2 movementInput = Vector2.zero;
-    //[SerializeField] float verticalInput;
-    //[SerializeField] float horizontalInput;
-
-    //[SerializeField] float rotationSpeed;
-
     private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
 
-    // Start is called before the first frame update
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
         if(isDashing) { return; }
         ShiftRun();
         PlayerMovement();
 
-        #region Flip CharacterSprite depending on Rotation
-
-        //if (horizontalInput != 0 && horizontalInput < 0)
-        //{
-        //    spriteRenderer.flipX = true;
-        //}
-        //else if (horizontalInput != 0 && horizontalInput > 0)
-        //{
-        //    spriteRenderer.flipX = false;
-        //}
-
-        #endregion Flip CharacterSprite depending on Rotation
     }
     private void FixedUpdate()
     {
@@ -95,8 +65,6 @@ public class playercomponent : MonoBehaviour
 
     private void RayCast()
     {
-        #region Old Code
-
         RaycastHit hit;
         Vector3 castPos = transform.position;
         castPos.y += 1;
@@ -105,36 +73,17 @@ public class playercomponent : MonoBehaviour
         {
             if (hit.collider != null)
             {
-                // transform.rotation =  Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
                 Vector3 movePos = transform.position;
                 movePos.y = hit.point.y + groundDistance;
                 transform.position = movePos;
             }
         }
-
-        #endregion Old Code
-
-        #region Deprecated
-
-        //Ray ray = new Ray(transform.position, -transform.up);
-        //RaycastHit hit;
-        //// if (Physics.Raycast(transform.position + Vector3.up, -Vector3.up, out RaycastHit, 1f + rayLength))
-        //if (Physics.Raycast(ray, out hit))
-        //{
-        //     //rotates the player in respect to the angle of the slope
-        //    transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-        //    Vector3 targetDir = (transform.position - hit.point).normalized;
-        //    transform.position = hit.point + targetDir * 1f;
-        //}
-
-        #endregion Deprecated
     }
 
     private bool OnSlope()
     {
         if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
         {
-            //transform.rotation = Quaternion.FromToRotation(transform.up, slopeHit.normal) * transform.rotation;
             float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
             return angle < maxSlopeAngle && angle != 0;
         }
@@ -164,19 +113,8 @@ public class playercomponent : MonoBehaviour
         }
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-        //it should be the 0 that's doing the issue
+
         moveDir = new Vector3(x, 0, y).normalized;
-
-        #region Jump
-        //if (Input.GetKeyDown(KeyCode.Space) && readyToJump)
-        //{
-        //    readyToJump = false;
-        //    Debug.Log("test");
-        //    Jump();
-        //    Invoke(nameof(ResetJump), jumpCoolDown);
-        //}
-        #endregion
-
         if (moveDir.magnitude >= 0.1f)
         {
             MoveDirection();
@@ -221,73 +159,4 @@ public class playercomponent : MonoBehaviour
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
-    #region Jump Code
-    //private void Jump()
-    //{
-    //    exitingSlope = true;
-    //    rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-    //    rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    //}
-
-    //private void ResetJump()
-    //{
-    //    readyToJump = true;
-    //    exitingSlope = false;
-    //}
-    #endregion
-
-    #region Old Movement System
-
-    //private void FixedUpdate()
-    //{
-    //    HandleAllMovement();
-    //}
-    //private void HandleAllMovement()
-    //{
-    //    MovePlayer();
-    //    HandleRotation();
-    //}
-    //public void OnMove(InputAction.CallbackContext value)
-    //{
-    // movementInput = value.ReadValue<Vector2>();
-    //}
-    //public void MovePlayer()
-    //{
-    //    Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-    //    movement.y = 0;
-    //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(TargetDirection()), 1f);
-    //    transform.Translate(MovementDirection(), Space.World);
-    //    Debug.Log(movement);
-    //}
-    //private Vector3 MovementDirection()
-    //{
-    //    Vector3 camForward = cameraObject.forward;
-    //    Vector3 camRight = cameraObject.right;
-    //    camForward.y = 0;
-    //    camRight.y = 0;
-
-    //    Vector3 forwardRelative = verticalInput * camForward;
-    //    Vector3 rightRelative = horizontalInput * camRight;
-    //    Vector3 movementDirection = forwardRelative + rightRelative;
-    //    movementDirection = movementDirection.normalized * speed * Time.deltaTime;
-    //    return movementDirection;
-    //}
-    //private Vector3 TargetDirection()
-    //{
-    //    Vector3 targetDirection;
-    //    targetDirection = cameraObject.forward * verticalInput;
-    //    targetDirection = targetDirection + cameraObject.right * horizontalInput;
-    //    targetDirection.Normalize();
-    //    targetDirection.y = 0f;
-    //    if (targetDirection == Vector3.zero) { targetDirection = transform.forward; }
-    //    return targetDirection;
-    //}
-    //private void HandleRotation()
-    //{
-    //    Quaternion targetRotation = Quaternion.LookRotation(TargetDirection());
-    //    Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed);
-    //    transform.rotation = playerRotation;
-    //}
-
-    #endregion Old Movement System
 }
